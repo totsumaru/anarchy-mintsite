999c79304882a237b5839a4f732ac52f7226bbb1
totsuka <techstart35@gmail.com>
2023-10-27 14:24:51 +0900

    fix

---
+++ app/_config/blockchain.ts
@@ -0,0 +1,5 @@
+import { Goerli } from "@thirdweb-dev/chains";
+
+export const activeChain = Goerli
+export const contractAddress = "0x10C5aE4EB0EfDbD592fF8a7C77f622d65885176D"
+export const symbol = "TEST"
